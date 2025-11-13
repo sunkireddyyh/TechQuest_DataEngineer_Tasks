b709e35a1cc9ff1009f55201c16baeb03d17fe88
sunkireddyyh <sunkireddyy.h@gmail.com>
2025-11-13 10:03:33 -0500

    Loaded Data into Sqlite3 db

--- Week4/Extraction.py
+++ Week4/Extraction.py
@@ -1,4 +1,6 @@
+import sqlite3
 import pandas as pd 
+
 
 df = pd.read_csv("EmployeeData copy.csv")
 
@@ -22,3 +24,16 @@ print("dropped df \n",df,"\n")
 df['Salary Hike'] = df["Salary"]*0.10
 print("added calculated field df \n",df)
 
+
+# Loading transformed data into Sqlite3 db 
+
+conn = sqlite3.connect("EmployeeData.db")
+
+df.to_sql("EmployeeData",conn,if_exists="replace",index=False)
+print("\n Data Successfully loaded into db")
+
+query = "SELECT * FROM EmployeeData"
+loaded_df = pd.read_sql_query(query,conn)
+print("\n Loaded Data from db \n",loaded_df,"\n")
+
+conn.close()
